1afc12018f0bda33a9d799b7b52d07ad915fb5a1
Chitwan Goel <101563468+chitwang@users.noreply.github.com>
2024-08-25 12:01:39 +0530

    Time: 273 ms (7.85%), Space: 123.2 MB (11.63%) - LeetHub

---
+++ 2435-shortest-impossible-sequence-of-rolls/2435-shortest-impossible-sequence-of-rolls.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int shortestSequence(vector<int>& rolls, int k) {
+        int n = rolls.size();
+        map<int, int> mp;
+        int ans = 0;
+        int cur = 0;
+        for(auto it:rolls){
+            mp[it]++;
+            if(mp[it] == 1){
+                cur++;
+            }
+            if(cur == k){
+                cur = 0;
+                mp.clear();
+                ans++;
+            }
+        }
+        return ++ans;
+    }
+};
