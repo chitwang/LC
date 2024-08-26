b7442ed4dcc96bccd315fe0d319e71b49566adb1
Chitwan Goel <101563468+chitwang@users.noreply.github.com>
2024-08-26 14:54:31 +0530

    Time: 496 ms (7.98%), Space: 179.6 MB (5.03%) - LeetHub

---
+++ 2176-parallel-courses-iii/2176-parallel-courses-iii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> toposort;
+    vector<vector<int>> adj;
+    vector<bool> vis;
+    void dfs(int i){
+        vis[i] = true;
+        for(auto it:adj[i]){
+            if(!vis[it])
+                dfs(it);
+        }
+        toposort.push_back(i);
+    } 
+    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+        adj.resize(n);
+        vis.resize(n, false);
+        vector<vector<int>> adj2(n);
+        for(auto it:relations){
+            it[0]--;it[1]--;
+            adj[it[0]].push_back(it[1]);
+            adj2[it[1]].push_back(it[0]);
+        }
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i);
+            }
+        }
+        reverse(toposort.begin(), toposort.end());
+        vector<int> dp(n);
+        for(auto i:toposort){
+            int maxprev = 0;
+            for(auto it:adj2[i]){
+                maxprev = max(maxprev, dp[it]);
+            }    
+            dp[i] = maxprev + time[i];
+        }      
+        return *max_element(dp.begin(), dp.end());
+    }
+};
